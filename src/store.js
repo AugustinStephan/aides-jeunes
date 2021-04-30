e5405a175496a1365fd993e1b654cc694d8ddd08
Clément Lelong <lelong@cymit.fr>
2021-04-30 11:45:53 +0200

    Enlève fullSteps du store

--- src/store.js
+++ src/store.js
@@ -58,25 +58,22 @@ function defaultCalculs() {
 function defaultStore() {
   const now = moment().format()
 
-  const defaultSituation = {
-      _id: null,
-      external_id: null,
-      dateDeValeur: now,
-      enfants: [],
-      famille: {},
-      logement: {},
-      foyer_fiscal: {},
-      menage: {
-          aide_logement_date_pret_conventionne: '2017-12-31'
-      },
-      version: 1,
-  }
-
   return {
     message: null,
     debug: false,
-    fullSteps: generateFullSteps(defaultSituation),
-    situation: defaultSituation,
+    situation: {
+        _id: null,
+        external_id: null,
+        dateDeValeur: now,
+        enfants: [],
+        famille: {},
+        logement: {},
+        foyer_fiscal: {},
+        menage: {
+            aide_logement_date_pret_conventionne: '2017-12-31'
+        },
+        version: 1,
+    },
     error: false,
     access: {
       fetching: false,
